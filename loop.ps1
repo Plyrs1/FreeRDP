34abb2c1a5a03e05508c3448115d99cfc0c6d667
Plyrs1 <shinobu13121@gmail.com>
2022-04-19 13:32:17 +0700

    Update loop.ps1
    recursive dir perms

--- loop.ps1
+++ loop.ps1
@@ -1,7 +1,7 @@
 $i = 360
 do {
     Start-Process '.\rclone\rclone.exe' '--config rclone.conf bisync googledrive: D:\RDP' 
-    Start-Process 'ICACLS' 'D:\RDP /grant FreeRDP:F'
+    Start-Process 'ICACLS' 'D:\RDP /grant FreeRDP:F /t'
     Write-Host "[+] Checking ngrok... $i"
     Get-Process -Name 'ngrok'
     Write-Host '[+] IP   :' ((Invoke-WebRequest -Uri http://localhost:4040/api/tunnels -ContentType "application/json").Content | ConvertFrom-Json).tunnels[0].public_url
