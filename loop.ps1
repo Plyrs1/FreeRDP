c50d64004eec2e2f2d68729a72da2bd37983e0e0
Plyrs1 <shinobu13121@gmail.com>
2022-04-19 13:27:35 +0700

    Update loop.ps1

--- loop.ps1
+++ loop.ps1
@@ -2,7 +2,9 @@ $i = 360
 do {
     Start-Process '.\rclone\rclone.exe' '--config rclone.conf bisync googledrive: D:\RDP' 
     Start-Process 'ICACLS' 'D:\RDP /grant FreeRDP:F'
-    Write-Host $i
+    Write-Host "[+] Checking ngrok... $i"
+    Get-Process -Name 'ngrok'
+    Write-Host '[+] IP   :' ((Invoke-WebRequest -Uri http://localhost:4040/api/tunnels -ContentType "application/json").Content | ConvertFrom-Json).tunnels[0].public_url
     Sleep 60
     $i--
 } while ($i -gt 0)
